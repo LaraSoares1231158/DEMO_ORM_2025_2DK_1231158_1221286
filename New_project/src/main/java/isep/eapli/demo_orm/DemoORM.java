8fea0a3902056a417263ecac5e2ed88121e4a131
LaraSoares <1231158@isep.ipp.pt>
2025-02-26 16:37:39 +0000

    Update DemoORM

--- New_project/src/main/java/isep/eapli/demo_orm/DemoORM.java
+++ New_project/src/main/java/isep/eapli/demo_orm/DemoORM.java
@@ -1,7 +1,10 @@
 package isep.eapli.demo_orm;
 
+import isep.eapli.demo_orm.apresentacao.MainMenu;
+
 public class DemoORM {
     public static void main(String[] args) {
+        MainMenu.mainLoop();
 
     }
 }
